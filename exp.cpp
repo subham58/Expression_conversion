db331ede49c1d381949c00e193da342c8d2f7267
Subham Mohanty <48358012+subham58@users.noreply.github.com>
2021-11-03 20:57:44 +0530

    Update_exp.cpp
    Comment removed of a program

--- exp.cpp
+++ exp.cpp
@@ -1,75 +1,75 @@
 // 1. infix to postfix conversion
 
-// #include<iostream>
-// #include<stack>
-// using namespace std;
+#include<iostream>
+#include<stack>
+using namespace std;
 
-// int prec(char c)
-// {
-//     if(c == '^')
-//     {
-//         return 3;
-//     }
-//     else if(c == '*' || c == '/') 
-//     {
-//         return 2;
-//     }
-//     else if(c == '+' || c == '-')
-//     {
-//         return 1;
-//     }
-//     else
-//     {
-//         return -1; // when there is opening parenthesis
-//     }
+int prec(char c)
+{
+    if(c == '^')
+    {
+        return 3;
+    }
+    else if(c == '*' || c == '/') 
+    {
+        return 2;
+    }
+    else if(c == '+' || c == '-')
+    {
+        return 1;
+    }
+    else
+    {
+        return -1; // when there is opening parenthesis
+    }
     
-// }
+}
 
-// string infixtoPostfix(string s)
-// {
-//     stack<char> st;
-//     string res;
+string infixtoPostfix(string s)
+{
+    stack<char> st;
+    string res;
 
-//     for(int i = 0; i < s.length(); i++)
-//     {
-//         if(s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z')
-//         {
-//             res+= s[i];
-//         }
-//         else if(s[i] == '(')
-//         {
-//             st.push(s[i]);
-//         }
-//         else if(s[i] == ')')
-//         {
-//             while(!st.empty() && st.top() != '(')
-//             {
-//                 res+= st.top();
-//                 st.pop();
-//             }
-//             if(!st.empty())
-//             {
-//                 st.pop();
-//             }
-//         }
-//         else  // for operator
-//         {
-//             while(!st.empty() && prec(st.top()) >= prec(s[i]))
-//             {
-//                 res+= st.top();
-//                 st.pop();
-//             }
-//             st.push(s[i]);
-//         }
-//     }
+    for(int i = 0; i < s.length(); i++)
+    {
+        if(s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z')
+        {
+            res+= s[i];
+        }
+        else if(s[i] == '(')
+        {
+            st.push(s[i]);
+        }
+        else if(s[i] == ')')
+        {
+            while(!st.empty() && st.top() != '(')
+            {
+                res+= st.top();
+                st.pop();
+            }
+            if(!st.empty())
+            {
+                st.pop();
+            }
+        }
+        else  // for operator
+        {
+            while(!st.empty() && prec(st.top()) >= prec(s[i]))
+            {
+                res+= st.top();
+                st.pop();
+            }
+            st.push(s[i]);
+        }
+    }
 
-//     while(!st.empty())
-//     {
-//         res+= st.top();
-//         st.pop();
-//     }
-//     return res;
-// }
+    while(!st.empty())
+    {
+        res+= st.top();
+        st.pop();
+    }
+    return res;
+}
 
 // int main()
 // {
